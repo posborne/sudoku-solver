b1585e9d5066ae622696cd1c096e2c8392df43d2
Paul Osborne <paul.osborne@spectrumdsi.com>
2009-11-18 21:50:49 -0600

    Fix working version with differential available table.
    This version does not have the naked singles optimization working

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,4 @@
+import puzzles
 import sys
 import array
 import copy
@@ -18,9 +19,18 @@ def pprint_puzzle(puzzle, empty='.'):
                 sys.stdout.write(' | ' + ' '.join(('%s' % i if i > 0 else empty for i in colset)))
             print ' |'
         print ROWSEP
+        
+def pprint_available(available):
+    i = 0
+    for i in xrange(9):
+        for j in xrange(9):
+            sys.stdout.write('%12s' % ','.join(('%d' % x for x in available[i][j])))
+        print ''
 
 def nums_available(puzzle, i, j):
     """Return all numbers available given a puzzle and i, j position"""
+    if puzzle[i][j] != 0:
+        return set()
     jrange = int(j / 3) * 3
     irange = int(i / 3) * 3
     colset = set((row[j] for row in puzzle))
@@ -29,105 +39,96 @@ def nums_available(puzzle, i, j):
     for row in (row[jrange:jrange + 3] for row in (colset for colset in puzzle[irange:irange + 3])):
         gridset.update([x for x in row])
     return set(xrange(1, 10)) - colset - rowset - gridset
-#    used_set = set()
-#    jrange = int(j / 3) * 3
-#    irange = int(i / 3) * 3
-#    for element in (row[j] for row in puzzle):
-#        used_set.add(element)
-#    for element in puzzle[i]:
-#        used_set.add(element)
-#    for row in (row[jrange:jrange + 3] for row in (colset for colset in puzzle[irange:irange + 3])):
-#        used_set.update([x for x in row])
-#    return set(range(1,10)) - used_set
+
+def build_available_table(puzzle):
+    """Build and return available table for puzzle"""
+    availaible = []
+    for i in xrange(9):
+        row = []
+        for j in xrange(9):
+            row.append(nums_available(puzzle, i, j))
+        availaible.append(row)
+    return availaible
+
+def update_from_position(puzzle, available, i, j):
+    """Update the available grid for those positions 
+    affected by the provided i, j position."""
+    num = puzzle[i][j]
+    jrange = int(j / 3) * 3 
+    irange = int(i / 3) * 3
+
+    # update rows and columns
+    for avail in ([row[j] for row in available] + available[i]):
+        avail.discard(num)
+    
+    for row in available[irange:irange + 3]:
+        for avail in row[jrange:jrange + 3]:
+            avail.discard(num)
+
+    return available
+
+def eliminate_naked_singles(puzzle, available, istart=0, jstart=0):
+    """Eliminate all naked singles"""
+    for i in xrange(istart, 9):
+        for j in xrange(jstart, 9):
+            avail = available[i][j]
+            if puzzle[i][j] == 0 and len(avail) == 1:
+                puzzle[i][j] = avail.pop()
+                update_from_position(puzzle, available, i, j)
+    return True
 
 def solve_puzzle(puzzle):
     """Do it now!"""
-    res = solve_puzzle_helper(puzzle, 0, 0)
+    available = build_available_table(puzzle)
+    res = solve_puzzle_helper(puzzle, available, 0, 0)
     if isinstance(res, tuple):
         return res[1]
     else:
         return False # no solutions found
 
-def solve_puzzle_helper(puzzle, i, j):
+def solve_puzzle_helper(puzzle, av, i, j):
     """Recursion, Oh Yeah"""
     next = (i, j + 1) if j < 8 else (i + 1, 0)
-    mypuzzle = copy.deepcopy(puzzle)
-    if not eliminate_naked_singles(mypuzzle):
-        return False # elimination detected a bad path
-    available = nums_available(mypuzzle, i, j)
+    available = av[i][j]
+    #sprint i, ",", j
+    #pprint_puzzle(puzzle)
+    #if not eliminate_naked_singles(mypuzzle, av):
+    #    return False
     if i == 8 and j == 8:
-        if len(available) == 1 or mypuzzle[i][j] != 0:
-            if (len(available) == 1):
-                mypuzzle[i][j] = list(available)[0]
-            eliminate_naked_singles(mypuzzle)
-            return True, mypuzzle
+        if len(available) == 1 or puzzle[i][j] != 0:
+            if len(available) == 1:
+                puzzle[i][j] = available.pop()
+            return True, puzzle
         else:
             return False
-    elif mypuzzle[i][j] != 0:
-        res = solve_puzzle_helper(puzzle, *next)
+    elif puzzle[i][j] != 0:
+        res = solve_puzzle_helper(puzzle, av, *next)
         if isinstance(res, tuple):
             return res
-    elif len(available) == 0 and mypuzzle[i][j] == 0:
+    elif len(available) == 0 and puzzle[i][j] == 0:
         return False
     else:
+        mypuzzle = copy.deepcopy(puzzle)
         for num in available:
+            locavail = copy.deepcopy(av)
             mypuzzle[i][j] = num
-            res = solve_puzzle_helper(mypuzzle, *next)
+            update_from_position(mypuzzle, locavail, i, j)
+            res = solve_puzzle_helper(mypuzzle, locavail, *next)
             if isinstance(res, tuple):
                 return res
     return False
 
-def eliminate_naked_singles(puzzle, istart=0, jstart=0):
-    """Eliminate all naked singles"""
-    for i in xrange(istart, 9):
-        for j in xrange(jstart, 9):
-            if puzzle[i][j] == 0:
-                avail = nums_available(puzzle, i, j)
-                if len(avail) == 0:
-                    return False
-                elif len(avail) == 1:
-                    puzzle[i][j] = list(avail)[0]
-                    # on average, faster without
-                    #eliminate_naked_singles(puzzle)
-    return True
-
-if __name__ == '__main__':
-    puzzle = \
-    [[0, 0, 5, 0, 0, 0, 0, 0, 1,],
-     [0, 0, 0, 9, 1, 0, 4, 0, 0,],
-     [0, 8, 0, 0, 0, 4, 0, 0, 0,],
-     [0, 0, 2, 0, 0, 7, 9, 0, 4,],
-     [0, 3, 0, 0, 0, 0, 0, 6, 0,],
-     [8, 0, 7, 2, 0, 0, 3, 0, 0,],
-     [0, 0, 0, 3, 0, 0, 0, 4, 0,],
-     [0, 0, 6, 0, 9, 5, 0, 0, 0,],
-     [4, 0, 0, 0, 0, 0, 2, 0, 0,]]
-#    puzzle = [ [0,0,0,8,0,0,0,0,0],
-#            [9,3,0,0,2,0,0,0,8],
-#            [0,0,4,0,0,6,2,9,3],
-#            [5,4,0,0,0,8,6,0,2],
-#            [0,2,3,0,0,0,5,8,0],
-#            [8,0,9,2,0,0,0,4,7],
-#            [2,6,8,4,0,0,9,0,0],
-#            [3,0,0,0,8,0,0,7,5],
-#            [0,0,0,0,0,3,0,0,0] ]
-#
-#    puzzle =[ [0,0,9,8,0,1,0,0,0],
-#            [6,0,3,0,0,0,0,9,0],
-#            [0,2,0,0,0,5,0,0,0],
-#            [9,4,0,0,0,0,0,0,0],
-#            [0,2,3,0,0,0,0,0,0],
-#            [0,0,0,2,0,0,0,0,0],
-#            [0,0,0,0,0,0,0,0,0],
-#            [0,0,0,0,8,0,0,0,0],
-#            [0,0,0,0,0,3,0,0,0] ]
-    newpuz = []
-    for row in puzzle:
-        newpuz.append(array.array('B', row))
-    puzzle = newpuz
+def solve_and_time(puzzle):
+    print ''
+    print '~' * 20 + ' PUZZLE ' + '~' * 20
     pprint_puzzle(puzzle)
     print '-' * 20 + ' SOLVING ' + '-' * 20
     stime = time.clock()
     puzzle = solve_puzzle(puzzle)
     print "Time: %s" % (time.clock() - stime)
     pprint_puzzle(puzzle)
+
+if __name__ == '__main__':
+    #solve_and_time(puzzles.puzzle0)
+    solve_and_time(puzzles.puzzle1)
+    solve_and_time(puzzles.puzzle2)
